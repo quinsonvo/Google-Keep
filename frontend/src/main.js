63457d3c139830cbe3b10f011ab44c499c530b44
Quinson Vo <votranquy96@gmail.com>
2025-05-06 21:52:05 +0700

    Use Toast as global

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// main.js
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 
-createApp(App).mount('#app')
+createApp(App).use(Toast).mount('#app')
